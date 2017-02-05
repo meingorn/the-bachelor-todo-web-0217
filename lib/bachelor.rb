f86ea23d61eca6137c395a6f64c1607440b83f9d
Max Eingorn <meingorn@gmail.com>
2017-02-05 18:01:11 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,56 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |data_hash, value|
+    data_hash.each do |data_point, detail|
+      if detail == "Winner"
+        return data_hash["name"].split.first
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, bachelors|
+    bachelors.each do |data_hash, data_point|
+      if data_hash["occupation"] == occupation
+        return data_hash["name"]
+        end
+      end
+    end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  i = 0
+  data.each do |season, bachelors|
+    bachelors.each do |data_hash, data_point|
+      if data_hash["hometown"] == hometown
+        i += 1
+      end
+    end
+  end
+  return i
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, bachelors|
+    bachelors.each do |data_hash, data_point|
+      if data_hash["hometown"] == hometown
+        return data_hash["occupation"]
+        break
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  i = 0
+  x = 0
+  data[season].each do |data_hash, value|
+    data_hash.each do |data_point, detail|
+      i += data_hash["age"].to_f
+      x += 1.to_f
+    end
+  end
+  return(i/x).round
 end
